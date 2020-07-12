93693928854aecdfdb52b81b54dc76f71c258ca5
Ye Joo Park <subwaymatch@gmail.com>
2020-07-11 20:53:35 -0500

    Change to a command line app

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const { organizeAssets } = require("./src/organize");
 
-const srcPath = "e:/Freepik/downloads";
-const outputPath = "e:/Freepik/organized";
+const chalk = require("chalk");
+const clear = require("clear");
+const figlet = require("figlet");
+const path = require("path");
 
-organizeAssets(srcPath, outputPath);
+console.log(
+    chalk.blue(
+        figlet.textSync("Organize.js", {
+            horizontalLayout: "default",
+        })
+    )
+);
+
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+    console.log(chalk.red("Missing input/output path"));
+    console.log(
+        chalk.magentaBright(
+            "Usage: node organize.js /path/to/input /path/to/output"
+        )
+    );
+    process.exit();
+}
+
+const inputArg = args[0];
+const outputArg = args[1];
+
+const inputPath = path.resolve(path.join(process.cwd(), inputArg));
+const outputPath = path.resolve(path.join(process.cwd(), outputArg));
+
+console.log(chalk.blue(`Source Directory: ${inputPath}`));
+console.log(chalk.blue(`Output Directory: ${outputPath}`));
+
+organizeAssets(inputPath, outputPath);
