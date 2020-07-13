6de396ab24407b176051d6b03538beccee7ec60e
Ye Joo Park <subwaymatch@gmail.com>
2020-07-13 00:33:15 -0500

    Fix incorrect usage example in cli

--- index.js
+++ index.js
@@ -6,23 +6,21 @@ const figlet = require("figlet");
 const path = require("path");
 
 console.log(
-    chalk.blue(
-        figlet.textSync("Organize.js", {
-            horizontalLayout: "default",
-        })
-    )
+  chalk.blue(
+    figlet.textSync("Organize.js", {
+      horizontalLayout: "default",
+    })
+  )
 );
 
 const args = process.argv.slice(2);
 
 if (args.length < 2) {
-    console.log(chalk.red("Missing input/output path"));
-    console.log(
-        chalk.magentaBright(
-            "Usage: node organize.js /path/to/input /path/to/output"
-        )
-    );
-    process.exit();
+  console.log(chalk.red("Missing input/output path"));
+  console.log(
+    chalk.magentaBright("Usage: node index.js /path/to/input /path/to/output")
+  );
+  process.exit();
 }
 
 const inputArg = args[0];
